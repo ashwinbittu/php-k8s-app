ef341da65384a4945696aba01bdfef560f216cdf
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2023-07-18 20:42:12 +1000

    Update pr.sh

--- pr.sh
+++ pr.sh
@@ -16,6 +16,7 @@ echo "Opening a Pull Request"
 #  "title": "Updated Solar System Image"
 #}'
 
+echo $GITHUB_TOKEN | gh auth login --with-token
 gh pr create --assignee "@me" --base "main" --title "Updated PHP k8s App" --body "Updated deployment specification with a new image version."
 
 echo "Success"
